35e72703cdb6b23aba0daba83f2b7855eabd9617
demir005 <demirmah93@gmail.com>
2019-03-28 21:37:44 +0100

    Added First Model

---
+++ BlogManager/Models/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogManager.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(350)]
+        public string Summary { get; set; }
+
+        [Required]
+        [MaxLength(3500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime DateAndTime { get; set; }
+    }
+}
